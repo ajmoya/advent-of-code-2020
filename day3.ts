c5fe9ffbe4cd12a45dca260dcb91694534ad5565
Toni <aj.mobe@gmail.com>
2020-12-08 01:31:25 +0100

    first commit

---
+++ day3.ts
@@ -0,0 +1,5 @@
+function Day3PartOne() {}
+function Day3PartTwo() {}
+
+console.log("Número de passwords válidas en Part One del puzzle: ", Day3PartOne());
+console.log("Número de passwords válidas en Part Two del puzzle: ", Day3PartTwo());
